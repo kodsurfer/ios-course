c2da9904630c1e1c7ad00789d80688987970d92e
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-24 01:32:36 +0300

    Create stack.swift

---
+++ stack.swift
@@ -0,0 +1,11 @@
+struct Stack<Element> {
+    private var items: [Element] = []
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element? {
+        return items.popLast()
+    }
+}
