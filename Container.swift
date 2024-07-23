eaed96bbc4d8d312e21636538dc6d1f0aa65bd4b
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-24 01:39:24 +0300

    Update Container.swift

--- Container.swift
+++ Container.swift
@@ -3,3 +3,23 @@ protocol Container {
     mutating func add(_ item: Item)
     func remove() -> Item?
 }
+
+extension Stack: Container {
+    mutating func add(_ item: Element) {
+        push(item)
+    }
+    
+    func remove() -> Element? {
+        return pop()
+    }
+}
+
+extension Queue: Container {
+    func add(_ item: Element) {
+        enqueue(item)
+    }
+    
+    func remove() -> Element? {
+        return dequeue()
+    }
+}
