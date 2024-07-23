708b71c4f183285a2b2daece8a50c0ecb6dfb174
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-24 01:42:50 +0300

    Update Container.swift

--- Container.swift
+++ Container.swift
@@ -1,3 +1,15 @@
+struct Stack<Element> {
+    private var items: [Element] = []
+    
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    
+    mutating func pop() -> Element? {
+        return items.popLast()
+    }
+}
+
 class Queue<Element> {
     private var items: [Element] = []
     
