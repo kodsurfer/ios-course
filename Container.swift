06ea2f7bac4868daebd02cb37287897ca2c4a192
kodsurfer <86408225+kodsurfer@users.noreply.github.com>
2024-07-24 01:42:28 +0300

    Update Container.swift

--- Container.swift
+++ Container.swift
@@ -1,3 +1,19 @@
+class Queue<Element> {
+    private var items: [Element] = []
+    
+    func enqueue(_ item: Element) {
+        items.append(item)
+    }
+    
+    func dequeue() -> Element? {
+        if items.isEmpty {
+            return nil
+        } else {
+            return items.removeFirst()
+        }
+    }
+}
+
 protocol Container {
     associatedtype Item
     mutating func add(_ item: Item)
